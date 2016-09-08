b64299c072cd1235405d76f52a0250503610d123
chari <chiragsharma991@gmail.com>
2016-09-08 16:33:09 +0530

    Initial commit

---
+++ app/src/main/java/com/example/chari/mytest/BaseActivity.java
@@ -0,0 +1,29 @@
+package com.example.chari.mytest;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Chari on 9/7/2016.
+ */
+public class BaseActivity extends AppCompatActivity {
+    public boolean isNetworkOnline() {
+        boolean haveConnectedWifi = false;
+        boolean haveConnectedMobile = false;
+
+        ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo[] netInfo = cm.getAllNetworkInfo();
+        for (NetworkInfo ni : netInfo) {
+            if (ni.getTypeName().equalsIgnoreCase("WIFI"))
+                if (ni.isConnected())
+                    haveConnectedWifi = true;
+            if (ni.getTypeName().equalsIgnoreCase("MOBILE"))
+                if (ni.isConnected())
+                    haveConnectedMobile = true;
+        }
+        return haveConnectedWifi || haveConnectedMobile;
+
+}
+}
